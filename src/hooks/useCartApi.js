ab407cfca13e0ddd0c8c2cb11541410a24338e35
boyok <boyzestsumbayak@gmail.com>
2024-12-27 23:26:42 +0700

    make checkbox

--- src/hooks/useCartApi.js
+++ src/hooks/useCartApi.js
@@ -19,7 +19,7 @@ const useCartApi = () => {
 
   const updateCartItem = async (item, action) => {
     try {
-      const response = await Apifetch("PUT", `/cart/${item.id}`, {
+      await Apifetch("PUT", `/cart/quantity/${item.id}`, {
         quantity:
           action === "increment" ? item.quantity + 1 : item.quantity - 1,
       });
@@ -29,11 +29,23 @@ const useCartApi = () => {
     getCartItem();
   };
 
+  const updateIsChecked = async (item, action) => {
+    try {
+      await Apifetch("PUT", `/cart/checked/${item.id}`, {
+        is_checked:
+          action === "checked" ? (item.is_checked = 1) : (item.is_checked = 0),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return {
     data,
     isLoading,
     getCartItem,
     updateCartItem,
+    updateIsChecked,
   };
 };
 
